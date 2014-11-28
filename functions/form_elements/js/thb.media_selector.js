bf3a5486a9b2805c7d5815674fb23e6442c97ab0
Alex Koti <alexkoti@gmail.com>
2014-11-28 17:37:36 -0200

    Rascunho para o element media_selector
    Utiliza o novo modal de mídia do wp

---
+++ functions/form_elements/js/thb.media_selector.js
@@ -0,0 +1,113 @@
+/**
+ * WordPress media selector plugin.
+ * by THBThemes, thehappybit@gmail.com
+ * http://thbthemes.com
+ *
+ * Copyright (c) 2014 Andrea Gandino, Simone Maranzana
+ * Licensed under the GPL (http://www.gnu.org/licenses/quick-guide-gplv3.html) license.
+ *
+ * Version: 1.0, 08.28.2014
+ */
+;( function( $ ) {
+	window.THB_MediaSelector = function( options ) {
+		/**
+		 * Instance to self.
+		 */
+		var self = this;
+
+		/**
+		 * Configuration options.
+		 */
+		self.options = $.extend( {}, {
+			/**
+			 * Title of the media selection modal window.
+			 *
+			 * @type {String}
+			 */
+			title: "Media",
+
+			/**
+			 * Set to true to activate multiple selection.
+			 *
+			 * @type {Boolean}
+			 */
+			multiple: false,
+
+			/**
+			 * Restrict items by type.
+			 * Pick from: "", "image", "audio", "video".
+			 */
+			type: "",
+
+			/**
+			 * Text of the modal submit button.
+			 *
+			 * @type {String}
+			 */
+			button: "Select",
+
+			/**
+			 * Modal window close callback function. Fired when the modal
+			 * window is closed without confirming the selection.
+			 */
+			close: function() {},
+
+			/**
+			 * Modal window select callback function. Fired when the modal
+			 * window is closed confirming the selection. The passed
+			 * argument is an array containing the selected image(s).
+			 *
+			 * @param {Array} selection Array containing the selected image(s).
+			 */
+			select: function( selection ) {}
+		}, options );
+
+		self.frame = null;
+
+		/**
+		 * Open the WordPress Media Manager modal window. If an array of IDs is
+		 * specified, the modal window will pre-select the relative attachments.
+		 * If IDs are specified, the modal window will open itself on the "Browse"
+		 * tab instead of the "Upload" tab.
+		 *
+		 * @param  {Array} ids An array of attachment IDs.
+		 */
+		self.open = function( ids ) {
+			self.frame = wp.media( {
+				title: self.options.title,
+				button: { text: self.options.button },
+				library: {
+					type: self.options.type
+				},
+				multiple: self.options.multiple
+			} );
+
+			self.frame.on( "open", function() {
+				var selection = self.frame.state().get( "selection" );
+				selection.reset();
+
+				_.each( ids, function( id ) {
+					var attachment = wp.media.attachment( id );
+
+					attachment.fetch();
+					selection.add( attachment ? [ attachment ] : [] );
+				} );
+			} );
+
+			self.frame.on( "select", function() {
+				var selection = self.frame.state().get( "selection" ),
+					result = self.options.multiple ? selection.toJSON() : selection.first().toJSON();
+
+				self.options.select( result );
+				delete self;
+			} );
+
+			self.frame.on( "close", function() {
+				self.options.close();
+				delete self;
+			} );
+
+			self.frame.open();
+		};
+	}
+} )( jQuery );
---
+++ functions/form_elements/media_selector.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * MEDIA SELECTOR
+ * 
+ * 
+ * 
+ */
+
+class BFE_media_selector extends BorosFormElement {
+	var $valid_attrs = array(
+		'name' => '',
+		'value' => '',
+		'id' => '',
+		'class' => '',
+		'rel' => '',
+		'placeholder' => '',
+		'size' => false,
+		'disabled' => false,
+		'readonly' => false,
+		'maxlength' => false,
+	);
+	
+	var $enqueues = array(
+		'js' => 'thb.media_selector',
+	);
+	
+	function set_input( $value = null ){
+		ob_start();
+		$input = ob_get_contents();
+		ob_end_clean();
+		?>
+		<input type='button' value='Selecionar Imagem' id='media_selector_test' class='button' />
+		<script type="text/javascript">
+		jQuery(document).ready(function($){
+			$('#media_selector_test').click(function(){
+				var media = new THB_MediaSelector({
+					title : 'Selecionar Imagem',
+					type : 'image',
+					multiple : true,
+					button : 'Definir imagens',
+					close : function(){
+						console.log('Media Selector fechada');
+					},
+					select: function( selected_images ){
+						console.log( selected_images );
+					}
+				});
+				media.open();
+			});
+		});
+		</script>
+		<?php
+		return $input;
+	}
+}
