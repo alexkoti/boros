fd2d27e6d03f01da2b9915d38363f8439e1a1510
Alex Koti <alexkoti@gmail.com>
2015-01-22 01:55:46 -0200

    Batch para criar os symlinks dos plugins mais usados localmente

---
+++ mklink-plugins.bat
@@ -0,0 +1,15 @@
+@echo off
+
+:: Este batch script cria junction links(symlinks) para as pastas dos plugins mais comuns no desenvolvimento localhost
+:: As pastas sempre serão apontadas para a pasta 'pluginsFolder', que é onde estão concentrados todos os plugins
+
+set "jobPath="
+set /p jobPath="Digite o caminho da raiz do job:"
+set "pluginsFolder=D:\PHP\xampp\htdocs\xampp\sites\boros\boros\wp-content\plugins"
+set "list=force-regenerate-thumbnails wp-email-login wp-quick-pages better-lorem boros-newsletter-extended user-role-editor"
+
+for %%i in (%list%) do (
+	mklink /d "%jobPath%\%%i" "%pluginsFolder%\%%i"
+)
+
+pause
