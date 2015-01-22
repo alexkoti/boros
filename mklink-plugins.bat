ba30d3a299e05cb436702fece40485a28f185481
Alex Koti <alexkoti@gmail.com>
2015-01-22 02:02:21 -0200

    Batch para criar um symlink de um único plugin.
    No prompt é perguntado o caminho da pasta de plugin do job e o slug do
    plugin a ser apontado.
    
    + correção no label da pergunta do batch de múltiplos plugins

--- mklink-plugins.bat
+++ mklink-plugins.bat
@@ -4,7 +4,7 @@
 :: As pastas sempre serão apontadas para a pasta 'pluginsFolder', que é onde estão concentrados todos os plugins
 
 set "jobPath="
-set /p jobPath="Digite o caminho da raiz do job:"
+set /p jobPath="Digite o caminho da pasta de plugin do job:"
 set "pluginsFolder=D:\PHP\xampp\htdocs\xampp\sites\boros\boros\wp-content\plugins"
 set "list=force-regenerate-thumbnails wp-email-login wp-quick-pages better-lorem boros-newsletter-extended user-role-editor"
 
