19f18be49e7b487aa9cfb33894a1d395d7a7dc04
Alex Koti <alexkoti@gmail.com>
2015-02-24 23:10:49 -0300

    Adição de timepicker ao elemento
    Foi iintegrado o timepicker, addon do datepicker padrão do jquery UI,
    mas ele é muito lento e congela por momentos o navegador, por isso foi
    adicionado o modificador 'split_time', para que se use campos de texto
    comum para hora e minuto quando for usar 'datetime'

--- functions/form_elements/date_picker.php
+++ functions/form_elements/date_picker.php
@@ -4,7 +4,12 @@
  * input text comum
  * 
  * 
- * @todo adicionar opções de mostrar vários meses, select de mês e ano, alternate field, restrict range, date range, opção de mostrar em um formato para usuário e salvar em outro em hidden(yyy-mm-dd)
+ * @todo adicionar opção de icon_trigger, restrict range e demais opções disponíveis nos pickers
+ * @todo traduzir as strings de time
+ * 
+ * Devido ao alto processamento do timepicker, foi deixado uma opção 'split_time', que quando definida para 'true' separa os
+ * campos de hora e minuto, cancelando também o uso do timepicker
+ * 
  * 
  */
 
@@ -30,8 +35,36 @@ class BFE_date_picker extends BorosFormElement {
 		'css' => array('date_picker'),
 	);
 	
-	function init(){
+	function localize_script(){
 		add_filter( 'admin_footer', array($this, 'localize') );
+	}
+	
+	/**
+	 * Opções
+	 * 
+	 * $type                - date | time | datetime
+	 * $split_time          - separar os campos de hora e minuto, cancelando também o uso do timepicker
+	 * $month_select        - dropdown de mês
+	 * $year_select         - dropdown de ano
+	 * $multiple_months     - mostrar múltiplos meses
+	 * $date_format         - formato de retorno da data
+	 * $time_format         - formato de retorno da hora
+	 * $step_minute         - intervalo de minutos dentro do select
+	 * $showSecond          - mostrar select de segundos
+	 * $date_range          - intervalo de datas
+	 * 
+	 */
+	function add_defaults(){
+		$this->defaults['options']['picker_type']         = 'date';
+		$this->defaults['options']['split_time']          = false;
+		$this->defaults['options']['months_number']       = 1;
+		$this->defaults['options']['date_format']         = 'dd/mm/yy';
+		$this->defaults['options']['time_format']         = 'hh:mm';
+		$this->defaults['options']['step_minute']         = 1;
+		$this->defaults['options']['month_select']        = false;
+		$this->defaults['options']['year_select']         = false;
+		$this->defaults['options']['show_second']         = false;
+		$this->defaults['options']['date_range']          = false;
 	}
 	
 	function localize(){
@@ -52,8 +85,9 @@ class BFE_date_picker extends BorosFormElement {
 				}
 				$locale_strings[$a] = $result;
 			}
-			$aryArgs = array(
-				'closeText'         => 'Fechar',
+			$date_picker_vars = array(
+				'dateFormat'        => $this->data['options']['date_format'],
+				'closeText'         => 'Pronto',
 				'currentText'       => 'Hoje',
 				'monthNames'        => $locale_strings['month'],
 				'monthNamesShort'   => $locale_strings['month_abbrev'],
@@ -63,7 +97,7 @@ class BFE_date_picker extends BorosFormElement {
 				'dayNamesMin'       => $locale_strings['weekday_initial'],
 				'firstDay'          => get_option( 'start_of_week' ),
 			);
-			wp_localize_script( 'form_element_date_picker_date_picker', 'datepickerL10n', $aryArgs );
+			wp_localize_script( 'form_element_date_picker_date_picker', 'datepickerL10n', $date_picker_vars );
 			self::$localized = true;
 		}
 	}
@@ -73,9 +107,74 @@ class BFE_date_picker extends BorosFormElement {
 	 * 
 	 */
 	function set_input( $value = null ){
-		$attrs = make_attributes($this->data['attr']);
+		$this->data['attr']['dataset']['picker_type'] = $this->data['options']['picker_type'];
+		// deixar o campo como date em vez de datetime, quando for utilizar o split_time
+		if( $this->data['options']['picker_type'] == 'datetime' and $this->data['options']['split_time'] === true ){
+			$this->data['attr']['dataset']['picker_type'] = 'date';
+		}
+		$this->data['attr']['dataset']['date_format']   = $this->data['options']['date_format'];
+		$this->data['attr']['dataset']['time_format']   = $this->data['options']['time_format'];
+		$this->data['attr']['dataset']['step_minute']   = $this->data['options']['step_minute'];
+		$this->data['attr']['dataset']['month_select']  = $this->data['options']['month_select'];
+		$this->data['attr']['dataset']['year_select']   = $this->data['options']['year_select'];
+		$this->data['attr']['dataset']['show_second']   = $this->data['options']['show_second'];
+		$this->data['attr']['dataset']['months_number'] = $this->data['options']['months_number'];
+		$this->data['attr']['dataset']['date_range']    = $this->data['options']['date_range'];
+		//pre($this->data['attr']['dataset']);
 		
-		$input = "{$this->input_helper_pre}<input type='text' value='{$value}'{$attrs} />{$this->input_helper}";
+		if( $this->data['options']['date_range'] === false ){
+			$value = boros_parse_args(array(
+				'view' => '',
+				'iso' => '',
+				'hour' => '',
+				'minute' => '',
+			), $value);
+		}
+		else{
+			$value = boros_parse_args(array(
+				'start_view' => '',
+				'start_iso' => '',
+				'start_hour' => '',
+				'start_minute' => '',
+				'end_view' => '',
+				'end_iso' => '',
+				'end_hour' => '',
+				'end_minute' => '',
+			), $value);
+		}
+		
+		ob_start();
+		//pre($value);
+		//pre($this->data['options'], 'options');
+		//pre($this->data['attr'], 'attr');
+		$name = $this->data['attr']['name'];
+		unset($this->data['attr']['name']);
+		$attrs = make_attributes($this->data['attr']);
+		//pal($attrs);
+		echo $this->input_helper_pre;
+		if( $this->data['options']['date_range'] === false ){
+			echo "<input type='text' value='{$value['view']}' name='{$name}[view]' {$attrs} />";
+			echo "<input type='hidden' value='{$value['iso']}' name='{$name}[iso]' class='input_date_picker_iso' />";
+			if( $this->data['options']['split_time'] === true ){
+				echo "<input type='number' value='{$value['hour']}' name='{$name}[hour]' min='0' max='24' maxlength='2' class='iptw_50' /> : <input type='number' value='{$value['minute']}' name='{$name}[minute]' min='0' max='59' maxlength='2' class='iptw_50' step='5' />";
+			}
+		}
+		else{
+			echo "<label for='{$this->data['attr']['id']}'>Início:</label> <input type='text' value='{$value['start_view']}' name='{$name}[start_view]' {$attrs} />";
+			echo "<input type='hidden' value='{$value['start_iso']}' name='{$name}[start_iso]' class='input_date_picker_iso' />";
+			if( $this->data['options']['split_time'] === true ){
+				echo "<input type='number' value='{$value['start_hour']}' name='{$name}[start_hour]' min='0' max='24' maxlength='2' class='iptw_50' /> : <input type='number' value='{$value['start_minute']}' name='{$name}[start_minute]' min='0' max='59' step='5' maxlength='2' class='iptw_50' />";
+			}
+			echo " &nbsp; <label for='{$this->data['attr']['id']}_end'>Fim:</label> <input type='text' value='{$value['end_view']}' name='{$name}[end_view]' id='{$this->data['attr']['id']}_end' class='input_date_picker_range_end' />";
+			echo "<input type='hidden' value='{$value['end_iso']}' name='{$name}[end_iso]' class='input_date_picker_iso_range_end' />";
+			if( $this->data['options']['split_time'] === true ){
+				echo "<input type='number' value='{$value['end_hour']}' name='{$name}[end_hour]' min='0' max='24' maxlength='2' class='iptw_50' /> : <input type='number' value='{$value['end_minute']}' name='{$name}[end_minute]' min='0' max='59' step='5' maxlength='2' class='iptw_50' />";
+			}
+		}
+		echo $this->input_helper;
+		
+		$input = ob_get_contents();
+		ob_end_clean();
 		
 		return $input;
 	}
