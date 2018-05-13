5c756ba866f94acbe65772d78e60162ad4459c2b
Alex Koti <alexkoti@gmail.com>
2018-05-13 19:44:18 -0300

    javascript para campos show/hiden dependente de select

--- functions/form_elements/js/form_elements.js
+++ functions/form_elements/js/form_elements.js
@@ -7,25 +7,49 @@
  */
 
 jQuery(document).ready(function($){
-	
-	/**
-	 * TAXONOMY CHECKBOX
-	 * 
-	 */
-	$('.force_hierachical .input_checkbox').change(function(){
-		var checkbox = $(this);
-		// caso seja um child, marcar o parent
-		if( checkbox.is('.force_hierachical .children li .input_checkbox') ){
-			var parent_check = checkbox.closest('.children').parent().find('.input_checkbox:first');
-			if( checkbox.is(':checked') ){
-				parent_check.attr('checked', true);
-			}
-		}
-		
-		// caso seja um parent, marcar os childs, se houver
-		if( checkbox.not(':checked') ){
-			checkbox.closest('li').find('.children .input_checkbox').attr('checked', false);
-		}
-	})
-	
+    
+    /**
+     * TAXONOMY CHECKBOX
+     * 
+     */
+    $('.force_hierachical .input_checkbox').change(function(){
+        var checkbox = $(this);
+        // caso seja um child, marcar o parent
+        if( checkbox.is('.force_hierachical .children li .input_checkbox') ){
+            var parent_check = checkbox.closest('.children').parent().find('.input_checkbox:first');
+            if( checkbox.is(':checked') ){
+                parent_check.attr('checked', true);
+            }
+        }
+        
+        // caso seja um parent, marcar os childs, se houver
+        if( checkbox.not(':checked') ){
+            checkbox.closest('li').find('.children .input_checkbox').attr('checked', false);
+        }
+    });
+    
+
+
+    /**
+     * Bloco duplicáveis com linhas condicionais, baseado no 'type' definido no primeiro select do bloco
+     * 
+     */
+    function boros_duplicate_element_opt_options( select_type, val ){
+        var obj = select_type.closest('.duplicate_element');
+        obj.find('.conditional-row').hide();
+        obj.find('.row-' + val).show();
+    }
+
+    // update onload/ on duplicate
+    $('.duplicate_group').bind('duplicate_group_complete sortcreate', function(event, ui) {
+        $(this).find('.conditional-type').each(function(){
+            boros_duplicate_element_opt_options( $(this), $(this).val() );
+        });
+    });
+
+    // update onchange do select
+    $('.duplicate-conditional').delegate('.conditional-type', 'change', function(){
+        boros_duplicate_element_opt_options( $(this), $(this).val() );
+    });
+    
 });
