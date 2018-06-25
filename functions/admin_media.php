443c638909a6f998ce3d291978994ff0219b696d
Alex Koti <alexkoti@gmail.com>
2018-06-24 22:56:52 -0300

    Normalizar filenames dos uploads, filtro padrão

--- functions/admin_media.php
+++ functions/admin_media.php
@@ -9,6 +9,45 @@
  * 
  */
 
+
+
+/**
+ * Normalizar filenames dos uploads
+ * 
+ * @link https://wpartisan.me/plugins/wpa-filename-sanitizer
+ * 
+ */
+add_filter( 'sanitize_file_name', 'boros_sanitize_file_name', 10, 1 );
+function boros_sanitize_file_name( $filename ) {
+
+    $sanitized_filename = remove_accents( $filename ); // Convert to ASCII
+
+    // Standard replacements
+    $invalid = array(
+        ' '   => '-',
+        '%20' => '-',
+        '_'   => '-',
+    );
+    $sanitized_filename = str_replace( array_keys( $invalid ), array_values( $invalid ), $sanitized_filename );
+
+    $sanitized_filename = preg_replace('/[^A-Za-z0-9-\. ]/', '', $sanitized_filename); // Remove all non-alphanumeric except .
+    $sanitized_filename = preg_replace('/\.(?=.*\.)/', '', $sanitized_filename); // Remove all but last .
+    $sanitized_filename = preg_replace('/-+/', '-', $sanitized_filename); // Replace any more than one - in a row
+    $sanitized_filename = str_replace('-.', '.', $sanitized_filename); // Remove last - if at the end
+    $sanitized_filename = strtolower( $sanitized_filename ); // Lowercase
+
+    /**
+     * Apply any more sanitization using this filter
+     * @var string $sanitized_filename The sanitized filename
+     * @var string $filename           Original filename
+     */
+    $sanitized_filename = apply_filters( 'boros_sanitize_file_name', $sanitized_filename, $filename );
+
+    return $sanitized_filename;
+}
+
+
+
 /**
  * Forçar o retorno da imagem original ao inserir no editor de texto
  * 
