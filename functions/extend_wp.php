27eb63d1df9b5cc55a9d50584c48fb35d896d276
Alex Koti <alexkoti@gmail.com>
2020-06-27 18:28:22 -0300

    retornar todos os tamanhos de imagem disponíveis

--- functions/extend_wp.php
+++ functions/extend_wp.php
@@ -811,6 +811,39 @@ class PageNaviList {
 
 
 /**
+ * Retornar todos os tamanhos de imagem
+ * 
+ * Get all the registered image sizes along with their dimensions
+ *
+ * @global array $_wp_additional_image_sizes
+ *
+ * @link http://core.trac.wordpress.org/ticket/18947 Reference ticket
+ *
+ * @return array $image_sizes The image sizes
+ * 
+ * @link https://wordpress.stackexchange.com/a/251602
+ * 
+ */
+function _get_all_image_sizes(){
+    global $_wp_additional_image_sizes;
+
+    $default_image_sizes = get_intermediate_image_sizes();
+
+    foreach ( $default_image_sizes as $size ) {
+        $image_sizes[ $size ][ 'width' ] = intval( get_option( "{$size}_size_w" ) );
+        $image_sizes[ $size ][ 'height' ] = intval( get_option( "{$size}_size_h" ) );
+        $image_sizes[ $size ][ 'crop' ] = get_option( "{$size}_crop" ) ? get_option( "{$size}_crop" ) : false;
+    }
+
+    if ( isset( $_wp_additional_image_sizes ) && count( $_wp_additional_image_sizes ) ) {
+        $image_sizes = array_merge( $image_sizes, $_wp_additional_image_sizes );
+    }
+
+    return $image_sizes;
+}
+
+
+/**
  * ==================================================
  * PAGINAÇÂO ========================================
  * ==================================================
