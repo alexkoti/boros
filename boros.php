c630a8355b44ba820f0b6d1e816ec408a0e74205
Alex Koti <alexkoti@gmail.com>
2020-09-25 18:49:32 -0300

    Versão plugin

--- boros.php
+++ boros.php
@@ -3,7 +3,7 @@
 Plugin Name: Boros Elements
 Plugin URI:  https://github.com/alexkoti/boros
 Description: Funções para o admin do WordPress, páginas personalizadas de administração(options) e campos de post_types(meta_boxes), widgets, form_elements e frontend forms
-Version:     1.5.4
+Version:     1.5.5
 Author:      Alex Koti
 Author URI:  http://alexkoti.com
 License:     GPL2
@@ -148,8 +148,6 @@ if( is_admin() ){
         'master'
     );
 }
-
-
 
 
 
