284f850ebe4aa114ebed7797262818ed74e8c24e
Alex Koti <alexkoti@gmail.com>
2020-11-02 23:54:57 -0300

    taxonomy_select2_ajax: controle de seleção de termos de taxonomia com busca em ajax

---
+++ functions/form_elements/js/taxonomy-select2-ajax.js
@@ -0,0 +1,64 @@
+
+jQuery(document).ready(function($){
+    
+    $('.input_taxonomy_select2_ajax').each(function(){
+        var select = $(this);
+        select.select2({
+            ajax: {
+                delay: 250,
+                url: ajaxurl,
+                data: function(params){
+                    return {
+                        action: 'boros_taxonomy_select2',
+                        taxonomy: select.attr('data-taxonomy'),
+                        post_id: select.attr('data-post_id'),
+                        number: select.attr('data-number'),
+                        term: params.term,
+                    }
+                },
+                dataType: 'json',
+            },
+            minimumInputLength: 3,
+            language: {
+                errorLoading: function () {
+                    return 'Os resultados não puderam ser carregados.';
+                },
+                inputTooLong: function (args) {
+                    var overChars = args.input.length - args.maximum;
+                    var message = 'Apague ' + overChars + ' caracter';
+                    if (overChars != 1) {
+                        message += 'es';
+                    }
+                    return message;
+                },
+                inputTooShort: function (args) {
+                    var remainingChars = args.minimum - args.input.length;
+                    var message = 'Digite ' + remainingChars + ' ou mais caracteres';
+                    return message;
+                },
+                loadingMore: function () {
+                    return 'Carregando mais resultados…';
+                },
+                maximumSelected: function (args) {
+                    var message = 'Você só pode selecionar ' + args.maximum + ' ite';
+                    if (args.maximum == 1) {
+                        message += 'm';
+                    } else {
+                        message += 'ns';
+                    }
+                    return message;
+                },
+                noResults: function () {
+                    return 'Nenhum resultado encontrado';
+                },
+                searching: function () {
+                    return 'Buscando…';
+                },
+                removeAllItems: function () {
+                    return 'Remover todos os itens';
+                }
+            }
+        });
+    });
+    
+});
---
+++ functions/form_elements/taxonomy_select2_ajax.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * TAXONOMY_SELECT2
+ * 
+ * Exemplo de configuração:
+ * <code>
+ *     array(
+ *         'name'    => 'tax_input[pleocroismo-x]',
+ *         'label'   => 'X',
+ *         'type'    => 'taxonomy_select2',
+ *         'size'    => 'large',
+ *         'attr'    => array(
+ *             'multiple' => 'multiple',
+ *         ),
+ *         'options' => array(
+ *             'taxonomy' => 'pleocroismo-x',
+ *         ),
+ *     )
+ * </code>
+ * 
+ * 
+ */
+
+class BFE_taxonomy_select2_ajax extends BorosFormElement {
+
+    var $valid_attrs = array(
+        'name'     => '',
+        'id'       => '',
+        'class'    => '',
+        'rel'      => '',
+        'disabled' => false,
+        'readonly' => false,
+    );
+    
+    var $enqueues = array(
+        'js'  => array('select2.min', 'taxonomy-select2-ajax'),
+        'css' => array('select2.min', 'taxonomy-select2-ajax'),
+    );
+    
+    function add_defaults(){
+        $this->defaults['options']['number']   = 6;
+        $this->defaults['options']['multiple'] = false;
+    }
+    
+    function set_input( $value = null ){
+        global $post;
+
+        $options  = wp_parse_args($this->data['options'], $this->defaults);
+        $taxonomy = isset($options['taxonomy']) ? $options['taxonomy'] : $this->data['name'];
+
+        // termos selecionados
+        $selected_terms = wp_get_object_terms( $post->ID, $taxonomy );
+
+        // usado pelo ajax, para identificar a taoxnomia
+        $this->data['attr']['dataset']['taxonomy'] = $taxonomy;
+        $this->data['attr']['dataset']['number']   = $options['number'];
+        if( $options['multiple'] == true ){
+            $this->data['attr']['multiple'] = 'multiple';
+        }
+        // atribuir corretamente o valor do POST
+        $this->data['attr']['name'] .= '[]';
+
+        $attrs = make_attributes($this->data['attr']);
+        
+        // output buffer
+        ob_start();
+        
+        echo "<select {$attrs}>";
+        if( !empty($selected_terms) ){
+            foreach( $selected_terms as $term ){
+                echo "<option value='{$term->term_id}' selected='selected'>{$term->name}</option>";
+            }
+        }
+        echo '</select>';
+        
+        $input = ob_get_contents();
+        ob_end_clean();
+        return $input;
+    }
+}
+
+/**
+ * Ajax da busca pelo select2
+ * 
+ */
+add_action( 'wp_ajax_boros_taxonomy_select2', 'ajax_boros_taxonomy_select2' );
+function ajax_boros_taxonomy_select2(){
+
+    if( empty($_GET['term']) ){
+        wp_send_json(array(
+            'total'   => 0,
+            'results' => array(),
+        ));
+    }
+
+    $terms = get_terms(array(
+        'taxonomy'   => $_GET['taxonomy'],
+        'search'     => $_GET['term'],
+        'hide_empty' => false,
+        'number'     => $_GET['number']
+    ));
+
+    if( !empty($terms) ){
+        $results = array();
+        foreach( $terms as $term ){
+            $results[] =  array(
+                'id'   => $term->term_id,
+                'text' => $term->name,
+            );
+        }
+        wp_send_json(array(
+            'total' => count($terms),
+            'results' => $results,
+        ));
+    }
+    else{
+        wp_send_json(array(
+            'total'   => 0,
+            'results' => array(),
+        ));
+    }
+
+    die();
+}
+
+
+
+
