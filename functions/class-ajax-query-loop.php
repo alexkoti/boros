a342f56f7f1478f207becde48348a683da7bded6
Alex Koti <alexkoti@gmail.com>
2020-12-11 20:09:06 -0300

    Melhorias em ajax_query_loop
    - métodos para início e fim do form, adicionar campos extras
    - enviar no ajax quaisquer campos extras definidos no formulário
    - exemplo de código completo

--- functions/class-ajax-query-loop.php
+++ functions/class-ajax-query-loop.php
@@ -6,6 +6,8 @@
  * Classe base para criar páginas de admin que utilizam loop para realizar tarefas em um grupo de posts através de 
  * queries complexas.
  * 
+ * @todo registrar um exemplo mais completo com campos extras e retorno
+ * 
 
 $args = array(
 	'page_args' => array(
@@ -23,25 +25,73 @@ $args = array(
 	),
 );
 
+
+Exemplo completo
+add_action( 'init', function(){
+
+    $args = array(
+        'page_args' => array(
+            'page_title' => 'Gerar Inscrições',
+            'menu_title' => 'Gerar Inscrições',
+            'menu_slug'  => 'generate-subs',
+            'parent'     => false,
+            'intro_html' => '<p>Arrumar o campo.</p><p>Offset: <input type="text" id="boros_ajax_query_loop_initial_offset" /></p>',
+        ),
+        'ajax_args' => array(
+            'action'         => 'jlpt_gen_subs',
+            'initial_offset' => 0,
+        ),
+        'query_args' => array(
+            'post_type'      => 'post',
+            'posts_per_page' => 1,
+            'post_status'    => 'any',
+        ),
+    );
+    
+    $gensubs = new JLPT_Generate_Subs( $args );
+
+} );
+
+class JLPT_Generate_Subs extends Boros_Ajax_Query_Loop {
+    
+    public function ajax(){
+
+        $offset = ($_POST['offset'] < 4 ) ? ($_POST['offset'] + 1) : 0;
+        $post_id = 1;
+
+
+        echo json_encode(array(
+            'offset'  => $offset,
+            'post_id' => $post_id,
+            'html'    => '<li>qwe qwe qweqwe</li>',
+        ));
+        die();
+    }
+}
+
+
  * 
  */
 
 abstract class Boros_Ajax_Query_Loop {
 	
 	protected $page_args = array(
-		'page_title' => 'Page title',
-		'menu_title' => 'Menu title',
-		'menu_slug'  => 'page-slug',
-		'parent'     => false,
-		'intro_html' => '<p>Page intro</p>',
+		'page_title'   => 'Page title',
+		'menu_title'   => 'Menu title',
+		'menu_slug'    => 'page-slug',
+		'parent'       => false,
+        'intro_html'   => '<p>Page intro</p>',
+        'offset_field' => true,
 	);
 	
 	protected $ajax_args = array(
-		'action' => 'boros_ajax_query_loop',
+		'action'         => 'boros_ajax_query_loop',
 		'initial_offset' => 0,
 	);
 	
-	protected $query_args = array();
+    protected $query_args = array();
+    
+    protected $offset_field = 'boros_ajax_query_loop_initial_offset';
 	
 	final public function __construct( $args ){
 		$this->page_args  = boros_parse_args( $this->page_args, $args['page_args'] );
@@ -73,7 +123,33 @@ abstract class Boros_Ajax_Query_Loop {
 	final public function add_submenu_page(){
 		add_submenu_page( $this->page_args['parent'], $this->page_args['page_title'], $this->page_args['menu_title'], 'activate_plugins', $this->page_args['menu_slug'], array($this, 'output') );
 		add_action( 'admin_print_footer_scripts', array($this, 'footer') );
-	}
+    }
+    
+    /**
+     * Formulário com campo de offset e campos adicionais
+     * 
+     */
+    final public function form(){
+        echo '<form onsubmit="return false;" id="boros-ajax-loop-form">';
+        $this->form_start();
+        $this->offset_field();
+        $this->form_end();
+        echo '</form>';
+    }
+
+    protected function offset_field(){
+        if( $this->page_args['offset_field'] == true ){
+            echo "<p>Offset: <input type='text' id='{$this->offset_field}' /></p>";
+        }
+    }
+
+    public function form_start(){
+        
+    }
+
+    public function form_end(){
+        
+    }
 	
 	/**
 	 * Adicionar scripts inline
@@ -96,18 +172,26 @@ abstract class Boros_Ajax_Query_Loop {
 							results.html('');
 							
 							// verificar se existe algum offset definido por campo de texto
-							if( $('#boros_ajax_query_loop_initial_offset').length && $('#boros_ajax_query_loop_initial_offset').val() > 0 ){
-								boros_ajax_query_loop_offset = $('#boros_ajax_query_loop_initial_offset').val();
+							if( $('#<?php echo $this->offset_field; ?>').length && $('#<?php echo $this->offset_field; ?>').val() > 0 ){
+								boros_ajax_query_loop_offset = $('#<?php echo $this->offset_field; ?>').val();
 								$('#boros-ajax-query-loop-results').attr('start', Number(boros_ajax_query_loop_offset) + 1);
 							}
 							boros_ajax_query_loop.proccess_item();
 						});
 					},
 					proccess_item : function(){
+
+                        var form_data = $('#boros-ajax-loop-form').serializeArray();
+
 						var data = {
 							action : '<?php echo $this->ajax_args['action']; ?>',
 							offset : boros_ajax_query_loop_offset,
 						};
+
+                        $(form_data).each(function(i, field){
+                            data[field.name] = field.value;
+                        });
+                        console.log(data);
 						
 						$.ajax({
 							type: 'POST',
@@ -201,6 +285,7 @@ abstract class Boros_Ajax_Query_Loop {
 		<div class="wrap">
 			<h1><?php echo $this->page_args['page_title']; ?></h1>
 			<?php echo $this->page_args['intro_html']; ?>
+            <?php $this->form(); ?>
 			<ol id="boros-ajax-query-loop-results" start="1"></ol>
 			<p class="submit">
 				<button id="boros-ajax-query-loop-submit" class="button button-primary">Iniciar</button>
