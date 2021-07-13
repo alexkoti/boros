36f55dc9739ce551592203c9bfce9a4138b596b5
Alex Koti <alexkoti@gmail.com>
2021-07-13 01:18:04 -0300

    modificadores de cores para pal, pam, sep + remoção de boros_current_page_info()
    + pam modificado para css inline
    + nova function bam para alert padrão bootstrap

--- functions/debug.php
+++ functions/debug.php
@@ -3,7 +3,6 @@
  * DEBUG FUNCTIONS
  * Funções para debug
  * 
- * @todo refatorar design/css
  * @todo adicionar bam() [Bootstrap Alert HTML] e modificar pam() para utilizar css próprio sem dependância de bootstrap
  * @todo em pam() verificar o enqueue de boostrap e apontar para bam()
  * @todo mover todas as chamadas de functions para métodos da class principal
@@ -16,7 +15,7 @@
 
 
 
-/**    
+/**
  * ==================================================
  * VERIFICAR FUNCTIONS DO HOOK ======================
  * ==================================================
@@ -73,38 +72,6 @@ function boros_current_screen_info() {
 
 /**
  * ==================================================
- * CURRENT PAGE INFO ================================
- * ==================================================
- * Mostrar dados da página corrent:
- *  - URL
- *  - template usado(arquivo .php)
- */
-//add_action( 'wp_footer', 'boros_current_page_info' );
-function boros_current_page_info(){
-    global $template, $current_user;
-    if( is_user_logged_in() ){
-        wp_get_current_user();
-        $show_debug = get_user_meta($current_user->ID, 'show_debug', true);
-        if(!empty($show_debug)){
-        ?>
-        <ul class="header_debug" 
-            style="position:fixed;width:100%;bottom:0;left:0;opacity:0.3;background:#FFFFE0;border:1px dotted #E6DB55;font-size:11px;margin:0;overflow:hidden;padding:4px;" 
-            onMouseOver="this.style.opacity='1'"
-            onMouseOut="this.style.opacity='0.3'"
-        >
-            <li><span title="Para trabalhar com forms">(?)</span> URL corrente: <code><?php echo self_url(); ?></code></li>
-            <li>Template-name: <code><?php echo basename($template); ?></code></li>
-            <li>Template-path: <code><?php echo $template; ?></code></li>
-        </ul>
-        <?php
-        }
-    }
-}
-
-
-
-/**
- * ==================================================
  * PRE ==============================================
  * ==================================================
  * Listar valores de um array ou objetct com <pre>
@@ -130,9 +97,9 @@ function pre( $var = false, $legend = '', $opened = true ){
  * @param mix $var_name exibir prefixo com o nome da variável, ou texto de introdução
  */
 if( !function_exists('pal') ){
-function pal( $var = false, $legend = '', $pad = 0 ){
+function pal( $var = false, $legend = '', $pad = 0, $level = 'warning' ){
 	$pre = PRE::init();
-	$pre->pal( $var, $legend, $pad );
+	$pre->pal( $var, $legend, $pad, $level );
 }
 }
 
@@ -148,6 +115,7 @@ function pal( $var = false, $legend = '', $pad = 0 ){
  */
 if( !function_exists('pcm') ){
 function pcm( $var = false, $legend = '', $pad = 0, $pad_pos = 'left' ){
+    $var = (string)$var;
     if( !empty($legend) ){
         if( $pad_pos == 'left' ){
             $legend = str_pad("{$legend} ", $pad, ' ', STR_PAD_LEFT);
@@ -162,7 +130,7 @@ function pcm( $var = false, $legend = '', $pad = 0, $pad_pos = 'left' ){
 }
 }
 
-/**    
+/**
  * ==================================================
  * ERROR LOG ========================================
  * ==================================================
@@ -185,18 +153,40 @@ function pel( $var = false, $legend = '', $pad = 0, $pad_pos = 'left' ){
 }
 }
 
-/**    
+/**
  * ==================================================
  * MENSAGEM HTML ====================================
  * ==================================================
- * {P}rint {A}lert {H}TML
+ * {P}rint {A}lert {M}essage
  * Aceita tags HTML
- * Está utilizando classes do bootstrap
  * 
- * @todo: embutir css
  */
 if( !function_exists('pam') ){
-    function pam( $message = '', $type = 'primary', $legend = '', $close_button = false ){
+    function pam( $message = '', $type = 'info', $legend = '' ){
+        $color = [
+            'neutral' => ['#383d41','#e2e3e5'],
+            'danger'  => ['#721c24','#f8d7da'],
+            'success' => ['#155724','#d4edda'],
+            'info'    => ['#004085','#cce5ff'],
+            'warning' => ['#856404','#fff3cd'],
+        ];
+        if( !empty($legend) ){
+            $legend = "<strong>{$legend}:</strong> ";
+        }
+        echo "<div style='background-color:{$color[$type][1]};border:1px solid;border-color:{$color[$type][0]}30;border-radius:4px;color:{$color[$type][0]};line-height:24px;margin:5px;position:relative;padding:12px 18px;'>{$legend}{$message}</div>";
+    }
+}
+
+/**
+ * ==================================================
+ * ALERTA BOOTSTRAP =================================
+ * ==================================================
+ * {B}ootstrap {A}lert {M}essage
+ * Aceita tags HTML
+ * 
+ */
+if( !function_exists('bam') ){
+    function bam( $message = '', $type = 'primary', $legend = '', $close_button = false ){
         if( !empty($legend) ){
             $legend = "<strong>{$legend}:</strong> ";
         }
@@ -216,8 +206,15 @@ if( !function_exists('pam') ){
  * 
  */
 if( !function_exists('sep') ){
-function sep( $message = '' ){
-	echo "<div style='background:red;color:#fff;font:12px normal arial, sans-serif;height:10px;line-height:10px;margin:50px 0;padding:5px;'>{$message}</div>";
+function sep( $message = '', $level = 'danger' ){
+    $color = [
+        'danger'  => '#dc3545',
+        'success' => '#28a745',
+        'info'    => '#17a2b8',
+        'warning' => '#ffae00',
+        'neutral' => '#6c757d',
+    ];
+    echo "<div style='background:{$color[$level]};border-radius:3px;color:#fff;font:12px fira code, monospace;height:11px;line-height:13px;margin:40px 5px;padding:10px;'>{$message}</div>";
 }
 }
 
@@ -235,15 +232,13 @@ if( !class_exists('PRE') ){
 class PRE {
 
     private $css = [
-        'pal'       => 'background:#F0F0BB;border:1px solid #666;border-left:10px groove #E08B8B;clear:both;color:#666;font:normal 12px fira code,monospace;line-height:130%;margin:5px;padding:5px;text-align:left;',
+        'pal'       => 'border:1px solid #666;border-left:10px groove;clear:both;color:#666;font:normal 12px fira code,monospace;line-height:130%;margin:5px;padding:5px;text-align:left;',
         'pre'       => 'background:#f4f4f4;border:1px solid #dfdfdf;border-radius:3px;clear:both;color:#333;font:10px fira code,monospace;line-height:130%;margin:5px;position:relative;text-align:left;',
         'pre_head'  => 'background:#ededed;color:#da1c23;cursor:pointer;font-size:12px;font-weight:normal;margin:0 !important;padding:5px;',
         'pre_value' => 'font-size:12px;color:#333;margin:0;padding:5px;',
         'pre_tag'   => 'border:none;color:#6B7688;font-size:12px;white-space:pre-wrap !important;margin:0;padding:5px;',
         'pre_foot'  => 'font-size:11px;background:#ededed;color:#999;margin:0;padding:5px;',
     ];
-
-    var $controls_visible = false;
     
     private static $instance;
 
@@ -260,13 +255,20 @@ class PRE {
 
     }
     
-    function pal( $message, $var_name = false, $pad = 0  ){
+    function pal( $message, $var_name = false, $pad = 0, $level = 'warning' ){
         $legend = '';
+        $color = [
+            'danger'  => ['#f8d7da','#d65661'],
+            'success' => ['#d4edda','#6ac580'],
+            'info'    => ['#d1ecf1','#59b6c7'],
+            'warning' => ['#f0f0bb','#e08b8b'],
+            'neutral' => ['#e2e3e5','#a5a5a5'],
+        ];
         if( !empty($var_name) ){
             $legend = $this->mb_str_pad("{$var_name} ", $pad, '-', STR_PAD_RIGHT);
             $legend = "<strong style='font-weight:600;'>{$legend}-&gt;</strong> ";
         }
-        echo PHP_EOL . "<div style='{$this->css['pal']}'>" . $legend . $this->esc_html($message) . "</div>\n";
+        echo PHP_EOL . "<div style='{$this->css['pal']};background-color:{$color[$level][0]};border-color:{$color[$level][1]}'>" . $legend . $this->esc_html($message) . "</div>\n";
     }
     
     public function pre( $var = false, $legend = '', $opened = true ){
