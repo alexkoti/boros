061f5fcd53a99c77253a7df47e85c9b75a0ee0e4
Alex Koti <alexkoti@gmail.com>
2022-02-25 01:44:51 -0300

    remover debug do autoload

--- functions/autoload.php
+++ functions/autoload.php
@@ -12,10 +12,8 @@ function boros_autoload_class( $class ){
 
     // Apenas classes Boros, sem namespace
     if( false !== strpos( $class, 'Boros_' ) && false === strpos( $class, 'Boros\\') ){
-        pel('autoload old');
         $classes_dir = BOROS_FUNCTIONS . DIRECTORY_SEPARATOR;
         $class_file = strtolower( str_replace( array('Boros', '_'), array('class', '-'), $class ) ) . '.php';
-        pel($class_file);
         require_once( $classes_dir . $class_file );
     }
     
