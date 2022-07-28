8aed6b2fd4f6a35e330d5a0c6e2098f736fd757e
Alex Koti <alexkoti@gmail.com>
2022-07-28 16:38:12 -0300

    remover correção curl, ajustes correção query monitor

--- functions/temp-fixes.php
+++ functions/temp-fixes.php
@@ -19,41 +19,19 @@
  * O query monitor espera que jquery seja carregado no header, o que causa erro na exibição do painel.
  * 
  */
-add_action( 'wp_default_scripts', 'fix_query_monitor_frontend', 101 );
-function fix_query_monitor_frontend($wp_scripts){
+add_action('wp_default_scripts', function(){
+    global $wp_scripts;
+
+    // não precisa verificar a declaração de 'query-monitor', pois o método WP_Dependencies->add_data() já faz essa verificação
     $wp_scripts->add_data( 'query-monitor', 'group', 1 );
-}
-
-
-
-/**
- * By default, cURL sends the "Expect" header all the time which severely impacts
- * performance. Instead, we'll send it if the body is larger than 1 mb like
- * Guzzle does.
- * 
- * @link https://gist.github.com/carlalexander/c779b473f62dcd1a4ca26fcaa637ec59
- */
-add_filter('http_request_args', 'add_expect_header');
-function add_expect_header(array $arguments){
-    $arguments['headers']['expect'] = '';
     
-    if (is_array($arguments['body'])) {
-        $bytesize = 0;
-        $iterator = new RecursiveIteratorIterator(new RecursiveArrayIterator($arguments['body']));
-
-        foreach ($iterator as $datum) {
-            $bytesize += strlen((string) $datum);
-
-            if ($bytesize >= 1048576) {
-                $arguments['headers']['expect'] = '100-Continue';
-                break;
-            }
+    // forçar a ordem de carregamento do query-monitor para ser o último da lista, pois acontece conflito com woocommerce e outros scripts
+    foreach( $wp_scripts->queue as $index =>$script ) {
+        if( $script == 'query-monitor' ){
+            unset($wp_scripts->queue[$index]);
+            $wp_scripts->queue[99] = 'query-monitor';
         }
-    } elseif (!empty($arguments['body']) && strlen((string) $arguments['body']) > 1048576) {
-        $arguments['headers']['expect'] = '100-Continue';
     }
-
-    return $arguments;
-}
+}, 101);
 
 
