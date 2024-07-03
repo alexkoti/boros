f8af09b9e604f8527a0adb8231d1003db06ca351
Alex Koti <alexkoti@gmail.com>
2024-07-03 18:06:21 -0300

    classes padronizadas para body

--- functions/frontend_static.php
+++ functions/frontend_static.php
@@ -76,14 +76,17 @@ function new_post_class( $classes, $class, $post_id ){
 
 add_filter( 'body_class', 'new_body_class' );
 function new_body_class( $classes ){
-	global $post;
-	$classes[] = "type-{$post->post_type}";
-	$classes[] = "post-type-{$post->post_type}";
-	
-	if( isset($post->post_name) ){
-		$classes[] = "item-name-{$post->post_name}";
-	}
-	return $classes;
+    global $post;
+
+    if( isset($post->post_type) ){
+        $classes[] = "type-{$post->post_type}";
+        $classes[] = "post-type-{$post->post_type}";
+    }
+    
+    if( isset($post->post_name) ){
+        $classes[] = "item-name-{$post->post_name}";
+    }
+    return $classes;
 }
 
 
